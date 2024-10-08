ea43e7be0e9331c66ade2b79b3a24362c010e763
LeyKoz <lucas.gonthier@etud.u-picardie.fr>
2024-10-08 15:22:59 +0200

    D - 6 : implémentation des ressources

--- Dev/main.js
+++ Dev/main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import Phaser from 'phaser'
+import Ressource from './Ressource.js'
+import Farmable from './Farmable.js'
 
 class GameScene extends Phaser.Scene{
   constructor(){
@@ -7,11 +9,22 @@ class GameScene extends Phaser.Scene{
     this.player
     this.cursor
     this.playerSpeed = 200
+	this.farmableGroup
+	this.resources = {
+		"wood": new Ressource("wood"),
+		"stone": new Ressource("stone"),
+		"meat": new Ressource("meat")
+	}
+	this.resourcesGroup
   }
 
   preload(){
     //load les sprites, sons, animations
     this.load.image("player", "\assets\player.png")
+	this.load.image("tree", "/assets/tree.png");
+	this.load.image("wood", "/assets/wood.png");
+	this.load.image("stone", "/assets/stone.png");
+	this.load.image("meat", "/assets/meat.png");
   }
 
   create(){
@@ -20,6 +33,18 @@ class GameScene extends Phaser.Scene{
     this.player.setImmovable(true)
     this.player.body.allowGravity = false
     this.cursor = this.input.keyboard.createCursorKeys()
+
+	// Créer le groupe de farmables
+	this.farmableGroup = this.physics.add.group();
+	// Initialiser le groupe des ressources
+    this.resourceGroup = this.physics.add.group();
+
+	// Ajouter des farmables
+	this.createFarmable('tree', 200, 200);
+	this.createFarmable('tree', 300, 300);
+
+	// Ajouter un overlap pour détecter l'interaction
+	this.physics.add.overlap(this.player, this.farmableGroup, this.hitFarmable, null, this);
   }
 
   update(){
@@ -51,9 +76,71 @@ class GameScene extends Phaser.Scene{
     }
   
     // Applique les vitesses au joueur
-    this.player.setVelocityX(velocityX);
-    this.player.setVelocityY(velocityY);
+    this.player.setVelocity(velocityX, velocityY);
   }
+
+  // Méthode pour collecter une ressource
+  collectResource(resourceType, quantity) {
+    if (this.resources[resourceType]) {
+      this.resources[resourceType].add(quantity);
+      console.log(`${quantity} ${resourceType} ajouté. Total : ${this.resources[resourceType].quantity}`);
+    }
+  }
+
+  createFarmable(type, x, y) {
+	// Créer un sprite farmable
+	const farmableSprite = this.farmableGroup.create(x, y, type);
+	farmableSprite.setOrigin(0.5, 0.5);
+
+	// Associer un objet Farmable au sprite
+	farmableSprite.farmableData = new Farmable(type, 5); // Par exemple : 5 HP pour un arbre
+	}
+
+	hitFarmable(player, farmableSprite) {
+		const farmable = farmableSprite.farmableData;
+
+		if (farmable) {
+			farmable.hit(); // Infliger des dégâts
+
+			console.log(`HP restant pour ${farmable.type}: ${farmable.hp}`);
+
+			if (farmable.isDestroyed()) {
+				console.log(`${farmable.type} détruit !`);
+				farmableSprite.destroy(); // Supprimer le farmable du jeu
+			}
+
+			this.createResource('wood', farmableSprite.x, farmableSprite.y);
+		}
+	}
+
+	createResource(type, x, y) {
+		// Générer des positions aléatoires autour du farmable
+		const offsetX = Phaser.Math.Between(-30, 30);  // Décalage horizontal aléatoire
+		const offsetY = Phaser.Math.Between(-30, 30);  // Décalage vertical aléatoire
+		const resourceX = x + offsetX;
+		const resourceY = y + offsetY;
+	
+		// Créer un sprite de ressource dans le groupe à la position générée
+		const resourceSprite = this.resourceGroup.create(resourceX, resourceY, type);
+		resourceSprite.setOrigin(0, 0);
+	
+		// Ajouter une physique de collision pour permettre la collecte
+		this.physics.add.overlap(this.player, resourceSprite, (player, resource) => {
+			// Quand le joueur marche sur la ressource, elle est collectée
+			this.collectResource(type, 1);  // Ajouter la ressource à la collection
+			resource.destroy();  // Supprimer la ressource visuelle après collecte
+		}, null, this);
+	}
+
+	collectResource(type, quantity) {
+        // Ajouter des ressources à la collecte globale
+        if (this.resources[type]) {
+            this.resources[type].add(quantity);
+            console.log(`Ressource ${type} collectée: ${quantity}, total: ${this.resources[type].quantity}`);
+        } else {
+            console.log(`Ressource ${type} non définie.`);
+        }
+    }
 }
 
 //config du jeu
