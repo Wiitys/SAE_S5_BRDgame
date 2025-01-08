7852492bbca826ef7edb7decd9517b1a5ec1a5cc
Vico0760 <victor.da.silva.l.m@gmail.com>
2025-01-08 18:23:22 +0100

    B - 4.2 : Script Insertion BDD
    Ajout de données dans le jeu de données

--- database/data.sql
+++ database/data.sql
@@ -6,12 +6,16 @@ VALUES
 -- Cartes
 INSERT INTO Maps (id_map, map_name)
 VALUES
-(1, 'Infernal Abyss'),
+(1, 'Infernal Abyss')
 
 
 -- Ressources
 INSERT INTO Ressources (id_ressource, ressource_name, category, value_food)
 VALUES
+(1, 'wood', 'ressource', 0),
+(2, 'stone', 'ressource', 0),
+(3, 'moltenIron', 'ressource', 0),
+(4, 'meat', 'food', 20)
 
 
 -- Farmables
@@ -27,7 +31,16 @@ VALUES
 -- Crafts
 INSERT INTO Crafts (id_craft, craft_name)
 VALUES
-
+(1, 'stick'),
+(2, 'plank'),
+(3, 'ironIngot'),
+(4, 'woodenAxe'),
+(5, 'woodenPickaxe'),
+(6, 'stonePickaxe'),
+(7, 'stoneAxe'),
+(8, 'ironSword'),
+(9, 'ironAxe'),
+(10, 'ironPickaxe')
 
 -- Armures
 INSERT INTO Armour (id_armour, armour_name, is_craftable, effect, resistance, id_craft)
@@ -35,9 +48,15 @@ VALUES
 
 
 -- Armes et outils
-INSERT INTO WeaponsTools (id_weapon, weapon_name, is_craftable, range_tool, angle, farmableDamage, attackDamage, id_craft)
+INSERT INTO WeaponsTools (id_weapon, weapon_name, is_craftable, quantity, range_tool, angle, farmableDamage, attackDamage, id_craft)
 VALUES
-
+(1, 'woodenAxe', TRUE, 1, 30, 60, 2, 2, 4),
+(2, 'woodenPickaxe', TRUE, 1, 60, 20, 3, 2, 5),
+(3, 'stonePickaxe', TRUE, 1, 45, 90, 4, 3, 6),
+(4, 'stoneAxe', TRUE, 1, 70, 90, 4, 3, 7),
+(5, 'ironSword', TRUE, 1, 90, 70, 0, 10, 8),
+(6, 'ironAxe', TRUE, 1, 70, 90, 8, 5, 9),
+(7, 'ironPickaxe', TRUE, 1, 45, 90, 8, 5, 10)
 
 -- Shop
 INSERT INTO Shop (id_item, item_name)
@@ -45,5 +64,8 @@ VALUES
 
 
 -- CraftRessources
-INSERT INTO CraftRessources (id_ressource, id_craft, id_craft_1, quantity_needed)
+INSERT INTO CraftRessources (id_ressource, id_craft, id_subcraft, quantity_out, quantity_needed)
 VALUES
+(1, 1, 0, 2, 1),
+(1, 2, 0, 4, 2),
+(3, 3, 0, 1, 2),
