dd2e48f7f33cb644133d174cfade7d0dcda76a45
Vico0760 <victor.da.silva.l.m@gmail.com>
2025-01-09 18:24:07 +0100

    B - 4.3 : Scripts BDD
    Ajout des scripts SQL pour tester le Docker avec la BDD créée

---
+++ bdd_SAE/1-init.sql
@@ -0,0 +1,152 @@
+CREATE TABLE Players(
+   id_player INT,
+   username VARCHAR(50),
+   Money INT,
+   level INT,
+   kills INT,
+   death INT,
+   boss_killed INT,
+   PRIMARY KEY(id_player),
+   UNIQUE(username)
+);
+
+CREATE TABLE Maps(
+   id_map INT,
+   map_name VARCHAR(100),
+   PRIMARY KEY(id_map)
+);
+
+CREATE TABLE Ressources(
+   id_ressource INT,
+   ressource_name VARCHAR(50),
+   category VARCHAR(50),
+   value_food INT,
+   PRIMARY KEY(id_ressource)
+);
+
+CREATE TABLE Servers(
+   id_server INT,
+   server_name VARCHAR(50),
+   PRIMARY KEY(id_server)
+);
+
+CREATE TABLE Shop(
+   id_item INT,
+   item_name VARCHAR(100),
+   PRIMARY KEY(id_item)
+);
+
+CREATE TABLE Crafts(
+   id_craft INT,
+   craft_name VARCHAR(50),
+   PRIMARY KEY(id_craft)
+);
+
+CREATE TABLE Parties(
+   id_party INT,
+   name_party VARCHAR(50),
+   is_private LOGICAL,
+   password VARCHAR(25),
+   id_map INT NOT NULL,
+   id_server INT NOT NULL,
+   PRIMARY KEY(id_party),
+   FOREIGN KEY(id_map) REFERENCES Maps(id_map),
+   FOREIGN KEY(id_server) REFERENCES Servers(id_server)
+);
+
+CREATE TABLE Ennemies(
+   id_ennemies INT,
+   name VARCHAR(255),
+   health_points INT,
+   type VARCHAR(50),
+   behavior VARCHAR(50),
+   attackRange INT,
+   searchRange INT,
+   actionDelay VARCHAR(50),
+   PRIMARY KEY(id_ennemies)
+);
+
+CREATE TABLE Farmables(
+   id_farmable INT,
+   type VARCHAR(50),
+   health_point INT,
+   PRIMARY KEY(id_farmable)
+);
+
+CREATE TABLE Armour(
+   id_armour INT,
+   armour_name VARCHAR(50),
+   is_craftable LOGICAL,
+   effect VARCHAR(50),
+   resistance INT,
+   id_craft INT NOT NULL,
+   PRIMARY KEY(id_armour),
+   UNIQUE(id_craft),
+   FOREIGN KEY(id_craft) REFERENCES Crafts(id_craft)
+);
+
+CREATE TABLE WeaponsTools(
+   id_weapon INT,
+   weapon_name VARCHAR(50),
+   is_craftable LOGICAL,
+   range_tool INT,
+   angle INT,
+   farmableDamage INT,
+   attackDamage INT,
+   id_craft INT NOT NULL,
+   PRIMARY KEY(id_weapon),
+   UNIQUE(id_craft),
+   FOREIGN KEY(id_craft) REFERENCES Crafts(id_craft)
+);
+
+CREATE TABLE PlayerShop(
+   id_player INT,
+   id_item INT,
+   item_cost INT,
+   PRIMARY KEY(id_player, id_item),
+   FOREIGN KEY(id_player) REFERENCES Players(id_player),
+   FOREIGN KEY(id_item) REFERENCES Shop(id_item)
+);
+
+CREATE TABLE FriendPlayer(
+   id_player INT,
+   id_player_1 INT,
+   PRIMARY KEY(id_player, id_player_1),
+   FOREIGN KEY(id_player) REFERENCES Players(id_player),
+   FOREIGN KEY(id_player_1) REFERENCES Players(id_player)
+);
+
+CREATE TABLE CraftRessources(
+   id_ressource INT,
+   id_craft INT,
+   id_craft_1 INT,
+   quantity_needed INT,
+   PRIMARY KEY(id_ressource, id_craft, id_craft_1),
+   FOREIGN KEY(id_ressource) REFERENCES Ressources(id_ressource),
+   FOREIGN KEY(id_craft) REFERENCES Crafts(id_craft),
+   FOREIGN KEY(id_craft_1) REFERENCES Crafts(id_craft)
+);
+
+CREATE TABLE PlayersParty(
+   id_player INT,
+   id_party INT,
+   PRIMARY KEY(id_player, id_party),
+   FOREIGN KEY(id_player) REFERENCES Players(id_player),
+   FOREIGN KEY(id_party) REFERENCES Parties(id_party)
+);
+
+CREATE TABLE dropTypeFarm(
+   id_ressource INT,
+   id_farmable INT,
+   PRIMARY KEY(id_ressource, id_farmable),
+   FOREIGN KEY(id_ressource) REFERENCES Ressources(id_ressource),
+   FOREIGN KEY(id_farmable) REFERENCES Farmables(id_farmable)
+);
+
+CREATE TABLE dropTypeMobs(
+   id_ressource INT,
+   id_ennemies INT,
+   PRIMARY KEY(id_ressource, id_ennemies),
+   FOREIGN KEY(id_ressource) REFERENCES Ressources(id_ressource),
+   FOREIGN KEY(id_ennemies) REFERENCES Ennemies(id_ennemies)
+);
---
+++ bdd_SAE/2-data.sql
@@ -0,0 +1,71 @@
+-- Serveurs
+INSERT INTO Servers (id_server, server_name)
+VALUES
+(1, 'EU');
+
+-- Cartes
+INSERT INTO Maps (id_map, map_name)
+VALUES
+(1, 'Infernal Abyss')
+
+
+-- Ressources
+INSERT INTO Ressources (id_ressource, ressource_name, category, value_food)
+VALUES
+(1, 'wood', 'ressource', 0),
+(2, 'stone', 'ressource', 0),
+(3, 'moltenIron', 'ressource', 0),
+(4, 'meat', 'food', 20)
+
+
+-- Farmables
+INSERT INTO Farmables (id_farmable, type, health_point, id_ressource)
+VALUES
+
+
+-- Ennemis
+INSERT INTO Ennemies (id_ennemies, name, health_points, type, behavior, attackRange, searchRange, actionDelay, id_ressource)
+VALUES
+(1, 'Boss', 50, 'Boss', 'Aggressive',  250, 400,  4000, NULL),
+
+-- Crafts
+INSERT INTO Crafts (id_craft, craft_name)
+VALUES
+(1, 'stick'),
+(2, 'plank'),
+(3, 'ironIngot'),
+(4, 'woodenAxe'),
+(5, 'woodenPickaxe'),
+(6, 'stonePickaxe'),
+(7, 'stoneAxe'),
+(8, 'ironSword'),
+(9, 'ironAxe'),
+(10, 'ironPickaxe')
+
+-- Armures
+INSERT INTO Armour (id_armour, armour_name, is_craftable, effect, resistance, id_craft)
+VALUES
+
+
+-- Armes et outils
+INSERT INTO WeaponsTools (id_weapon, weapon_name, is_craftable, quantity, range_tool, angle, farmableDamage, attackDamage, id_craft)
+VALUES
+(1, 'woodenAxe', TRUE, 1, 30, 60, 2, 2, 4),
+(2, 'woodenPickaxe', TRUE, 1, 60, 20, 3, 2, 5),
+(3, 'stonePickaxe', TRUE, 1, 45, 90, 4, 3, 6),
+(4, 'stoneAxe', TRUE, 1, 70, 90, 4, 3, 7),
+(5, 'ironSword', TRUE, 1, 90, 70, 0, 10, 8),
+(6, 'ironAxe', TRUE, 1, 70, 90, 8, 5, 9),
+(7, 'ironPickaxe', TRUE, 1, 45, 90, 8, 5, 10)
+
+-- Shop
+INSERT INTO Shop (id_item, item_name)
+VALUES
+
+
+-- CraftRessources
+INSERT INTO CraftRessources (id_ressource, id_craft, id_subcraft, quantity_out, quantity_needed)
+VALUES
+(1, 1, 0, 2, 1),
+(1, 2, 0, 4, 2),
+(3, 3, 0, 1, 2),
